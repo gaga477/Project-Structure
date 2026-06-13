d77d4519a576d9c71e8e9f05376c079a3749fd43
Ogaga Ejairu <ejairuogaga@gmail.com>
2026-06-13 14:02:51 +0100

    chore: commit - MongoDB connected

--- zunny-pos-backend/config/db.js
+++ zunny-pos-backend/config/db.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const uri = process.env.MONGODB_URI || process.env.MONGODB_URL;
+  const uri = process.env.MONGODB_URI || process.env.MONGODB_URL || process.env.MONGO_URI;
 
   try {
     console.log("Connecting to MongoDB...");
     console.log("Mongoose version:", mongoose.version);
-    console.log("MONGO URI:", process.env.MONGODB_URI);
+    console.log("MONGO URI:", uri ? "(hidden)" : undefined);
 
     await mongoose.connect(uri, {
       serverSelectionTimeoutMS: 10000,
